4bf611bb48fb0b9319d6fe05b3d7bf6a1c350817
tjs1016 <tjs1016@gmail.com>
2020-07-06 14:06:10 -0400

    update for final vec3 methods

--- vec3.h
+++ vec3.h
@@ -21,5 +21,26 @@ struct vec3
 	inline vec3(float* fv) : x(fv[0]), y(fv[1]), z(fv[2]) { }
 };
 
+
+vec3 operator+(const vec3& l, const vec3& r);
+vec3 operator-(const vec3& l, const vec3& r);
+vec3 operator*(const vec3& v, float f);
+vec3 operator*(const vec3& l, const vec3& r);
+float dot(const vec3& l, const vec3& r);
+float lenSq(const vec3& v);
+float len(const vec3& v);
+void normalize(vec3& v);
+vec3 normalized(const vec3& v);
+float angle(const vec3& l, const vec3& r);
+vec3 project(const vec3& a, const vec3& b);
+vec3 reject(const vec3& a, const vec3& b);
+vec3 reflect(const vec3& a, const vec3& b);
+vec3 cross(const vec3& l, const vec3& r);
+vec3 lerp(const vec3& s, const vec3& e, float t);
+vec3 slerp(const vec3& s, const vec3& e, float t);
+vec3 nlerp(const vec3& s, const vec3& e, float t);
+bool operator==(const vec3& l, const vec3& r);
+bool operator!=(const vec3& l, const vec3& r);
+
 #endif // !_H_VEC3_
 
